9a034b7cebb354075ac3470a44b873cab21bd2f9
Pluppen <eliass.juremalm@gmail.com>
2020-09-07 13:41:23 +0200

    add print

--- tests/webtest/website_tests.py
+++ tests/webtest/website_tests.py
@@ -106,7 +106,9 @@ class InformationTest(unittest.TestCase):
         open_hours = ['Måndagar 10-16', 'Tisdagar 10-16', 'Onsdagar 10-16', 'Torsdagar 10-16', 'Fredagar 10-16', 'Lördagar 12-15']
 
         opening_hours_elems = driver.find_elements(By.CLASS_NAME, "opening-hour")
+        print(opening_hours_elems)
         for open_hour, index in opening_hours_elems:
+            selpening_hours_elems:
             self.assertEqual(open_hour.text, open_hours[index])
 
 
