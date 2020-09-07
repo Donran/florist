9453ad691cbf2af643ee91d1e88034bae3095b8a
Pontus N <pontusnorr@gmail.com>
2020-09-07 13:10:14 +0200

    Updated CSS validator to use JSON

--- tests/validate/css_validator.sh
+++ tests/validate/css_validator.sh
@@ -1,49 +1,35 @@
 #!/bin/bash
 
-# Constant for the message return if no errors occured
-SUCCESS_MSG="No Error Found."
-
-
-# Make sure a directory to look in is provided
+# Constant for the mssage return if no errors occured
 if [ "$1" == "" ]; then
-    echo -e "Please provide the root folder as such:\n\t$0 public"
+    echo -e "Please provide the root folder as such\n\t$0 public"
     exit
 fi
 
-# Go through all files ending in .css 
-for i in $(find $1 -iname '*.css'); do
+# Go through all files ending in .css
+for file in $(find $1 -iname '*.css'); do
     # Send a request to the w3 API for validating CSS
     # files need to be submitted as a multipart/form-data
-    # Select the css3 profile and output as text/plain 
+    # Select the css3 profile and output as text/plain
     data=`curl -s -H "Content-Type: multipart/form-data" \
-        -F "text=<$i;type=text/plain" \
+        -F "text=<$file;type=text/plain" \
         -F "profile=css3" \
-        -F "output=text/plain" \
-            https://jigsaw.w3.org/css-validator/validator`
-    # If we find SUCCESS_MSG the file is valid
-    if echo $data | grep -q "$SUCCESS_MSG"; then
+        -F "output=json" \
+        https://jigsaw.w3.org/css-validator/validator`
+    json=`jq .cssvalidation <<< "$data"`
+    # Check if any errors occured
+    error_count=`echo $json | jq .result.errorcount`
+    if [ "$error_count" = "0" ]; then
         echo "Success for $i"
     else
-        # If the file is not valid, print all errors provided
-        echo "Found errors in file $i"
-        errors=$(echo $data | cut -c 112-)
-        IFS=':' read -ra err_arr <<< "$errors"
-        
-        # Print all errors        
-        for err in "${err_arr[@]}"
-        do
-            if [ "$err" == " Line " ]; then
-                echo "=============================="
-            else
-                echo "$err"
-            fi
+        # If errors are found, print them.
+        echo "Errors:"
+        for i in $(seq 0 `expr $error_count - "1"`); do
+            error=`echo $json | jq .errors[$i]`
+            echo "$(echo $error | jq -r .context): L$(echo $error | jq -r .line) {type: $(echo $error | jq -r .type)}"
+            echo -e "\t $(echo $error | jq -r .message)"
         done
-
-        echo "=============================="
-        # Return 1 as exit status because an error occured
         exit 1
     fi
-
 done
 exit 0
-
