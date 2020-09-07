863b7d5e1b8a7c1f54e079c7845287904df71cb5
Pontus N <pontusnorr@gmail.com>
2020-09-07 14:35:01 +0200

    Divide tests into files

---
+++ tests/webtest/test_information.py
@@ -0,0 +1,68 @@
+import WebTestBase
+from selenium.webdriver.common.by import By
+class InformationTest(WebTestBase.BaseTest):
+
+    # Tests if website contains address information
+    def test_address_found(self):
+        driver = self.driver
+        driver.get(self.WEBSITE_URL)
+        address = "Fjällgatan 32H 981 39 KIRUNA"
+
+
+        try:
+            driver.find_element(By.PARTIAL_LINK_TEXT, address)
+        except NoSuchElementException:
+            self.fail("No address found")
+
+
+    # Tests if website contains opening hours
+    def test_opening_hours_found(self):
+        driver = self.driver
+        driver.get(self.WEBSITE_URL)
+
+        open_hours = ['Måndagar 10-16', 'Tisdagar 10-16', 'Onsdagar 10-16', 'Torsdagar 10-16', 'Fredagar 10-16', 'Lördagar 12-15']
+
+        opening_hours_elems = driver.find_elements(By.CLASS_NAME, "opening-hour")
+
+        if(len(opening_hours_elems) > 0):
+            for open_hour, index in opening_hours_elems:
+                self.assertEqual(open_hour.text, open_hours[index])
+        else:
+            self.fail("No opening hours found")
+
+    # Tests if website contains email and phonenumber
+    def test_email_and_phonenumber_found(self):
+        driver = self.driver
+        driver.get(self.WEBSITE_URL)
+
+        email = "info@DOMÄN"
+        phonenumber = "0630-555-555"
+
+
+        try:
+            driver.find_element(By.PARTIAL_LINK_TEXT, email)
+        except NoSuchElementException:
+            self.fail("No email found")
+
+
+        try:
+            driver.find_element(By.PARTIAL_LINK_TEXT, phonenumber)
+        except NoSuchElementException:
+            self.fail("No phonenumber found")
+
+    # Test som kollar om hemsidan innehåller en
+    # <h1 class="title"> med texten "Välkommen till Floristgården!"
+    def test_text_exist(self):
+        validText = "Välkommen till Floristgården"
+        driver = self.driver
+        driver.get(self.WEBSITE_URL)
+        elem = driver.find_element(By.CLASS_NAME, "title")
+        self.assertEqual(elem.text, validText)
+
+
+    # Test som kollar om hemsidans title innehåller "Floristgården"
+    def test_title_exist(self):
+        driver = self.driver
+        driver.get(self.WEBSITE_URL)
+        validTitle = "Floristgården"
+        self.assertIn(validTitle, driver.title)
