4a3a99ad9bc106590be99537463dc28b1ef763e9
Pluppen <eliass.juremalm@gmail.com>
2020-09-08 09:54:18 +0200

    Add test for products

---
+++ tests/webtest/test_products.py
@@ -0,0 +1,44 @@
+import WebTestBase
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+class ImagesTest(WebTestBase.BaseTest):
+    # Test to make sure all links are present
+    def test_products(self):
+        driver = self.driver
+        driver.get(self.WEBSITE_URL)
+
+        products = driver.find_elements(By.CLASS_NAME, "product")
+
+        if(len(products) > 0):
+            # List with expected products, every list item is a product
+            # Use this format [<product_name>, <product_price>, <product_id>]
+            expected_products = [
+                ["Bröllopsbruketter", 1200, 4], 
+                ["Höstbukett", 400, 6], 
+                ["Sommarbukett", 200, 3], 
+                ["Rosor 10-pack", 150, 8], 
+                ["Tulpaner 10-pack", 100, 9], 
+                ["Konsultation 30 min", 250, 10], 
+                ["Begravningskrans", 800, 5]
+            ]
+
+            # Checks if the products are less than expected
+            if(len(products) < 7):
+                self.fail("Not all products could be found.")
+            else:
+                for product, index in products:
+                    expected_product = expected_products[index]
+                    expected_name = expected_product[0]
+                    expected_price = expected_product[1]
+                    expected_id = expected_product[2]
+
+                    name = product.find_element(By.CLASS_NAME, "product-name")
+                    price = product.find_element(By.CLASS_NAME, "price")
+                    product_id = product.get_attribute("id")
+
+                    self.assertEqual(name, expected_name)
+                    self.assertEqual(price, expected_price)
+                    self.assertEqual(product_id, expected_id)
+        else:
+            self.fail("No products found")
+
