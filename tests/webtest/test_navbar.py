a5fd91ef3374736c264103f7d44d6bc3cab34a7c
Pontus N <pontusnorr@gmail.com>
2020-09-09 13:40:10 +0200

    Updated navbar test
    Now tests the links as well

--- tests/webtest/test_navbar.py
+++ tests/webtest/test_navbar.py
@@ -1,22 +1,29 @@
 import WebTestBase
+import requests
+from os import path
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
 class NavbarTest(WebTestBase.BaseTest):
     def test_navbar(self):
         driver = self.driver
         driver.get(self.WEBSITE_URL)
-        required_links = [
-            "Hem",
-            "Personal",
-            "Hitta Hit"
-        ]
+        required_links = {
+            "Hem": "index.html",
+            "Personal": "personal.html",
+            "Hitta Hit": "hitta_hit.html"
+        }
         navbar_items = driver.find_elements(By.CLASS_NAME, "nav-item")
         found = 0
         for item in navbar_items:
             link = item.find_element(By.CLASS_NAME, "nav-link")
             href = link.get_attribute("href") ## make sure all links are visitable
             name = link.text
+            file_name = path.basename(href)
+            if file_name not in required_links.values():
+                self.fail("Could not find required link: {}".format(file_name))
+            r = requests.get(href)
+            if r.status_code == 404:
+                self.fail("Could not find link: {}".format(href))
             if name in required_links:
                 found += 1
-
         self.assertGreaterEqual(len(required_links), found)
+
