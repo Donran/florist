02563d7559fc0e371806ae8fd3ed82de35fcde15
Albin Nyman <joshei@telia.com>
2020-09-10 10:09:59 +0200

    Updated test to check for icons and not only links

--- tests/webtest/test_socialmedia.py
+++ tests/webtest/test_socialmedia.py
@@ -4,9 +4,23 @@ from selenium.webdriver.common.by import By
 
 class SocialMediaTest(WebTestBase.BaseTest):
 
-    # Tests if the website contains the social media icons 
-    # that links to Floristgården's social medias
-    def test_socialmedia(self):
+
+    # Tests if the website contains the social 
+    # media icons
+    def test_socialmedia_icons(self):
+        driver = self.driver
+        driver.get(self.WEBSITE_URL)
+        icons = driver.find_elements(By.CLASS_NAME, "fab")
+        found = 0
+
+        for item in icons:
+            found += 1
+
+        self.assertEqual(found, 3)
+
+    # Tests if the icon links work and direct
+    # the user to the correct website
+    def test_socialmedia_links(self):
         driver = self.driver
         driver.get(self.WEBSITE_URL)
         required_links = [
@@ -14,9 +28,9 @@ class SocialMediaTest(WebTestBase.BaseTest):
             "https://twitter.com/ntiuppsala",
             "https://www.instagram.com/ntiuppsala/"
         ]
-        icons = driver.find_elements(By.CLASS_NAME, "social-link")
+        links = driver.find_elements(By.CLASS_NAME, "social-link")
         found = 0
-        for item in icons:
+        for item in links:
             href = item.get_attribute("href") ## Gets the link 
             if href not in required_links:
                 self.fail("Could not find required link: {}".format(href))
